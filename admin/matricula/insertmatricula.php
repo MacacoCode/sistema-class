87872081e4574f3c5195e9dfa3840e9f2ded8f2c
unknown <josuegrs1998.jr@gmail.com>
2019-05-21 10:47:57 -0600

    a

--- admin/matricula/insertmatricula.php
+++ admin/matricula/insertmatricula.php
@@ -3,8 +3,14 @@ include('../../conexion.php');
 	
 	//recuperar las variables
 	$idalumno=$_POST['idalumno'];
-	$nombre=$_POST['nombre'];
+	$nombre=$_POST['nombre']; //id de la materia
 
+	//prueba para que no choquen las clases, hay que hacer las variables de insertmaterias globales?????
+	/*
+	$sinchoque = "SELECT count(*) as sinchoque from materias, hora_materia, materias_alumnos
+	where materias.idmateria = hora_materia.idmateria and materias.idmateria= materias_alumnos.idmateria
+	and hora_materia.horainicio ='10' and hora_materia.horfinal='12' and hora_materia.dia='Martes'  and materias_alumnos.idalumno='$idalumno';"
+	*/
 
 	//hacemos la sentencia de sql
 	$sql="INSERT INTO materias_alumnos(idalumno, idmateria) VALUES('$idalumno','$nombre')";
