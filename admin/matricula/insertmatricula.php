54b61a2046c0127934c4b54632021fcb27c10c32
unknown <josuegrs1998.jr@gmail.com>
2019-05-22 10:20:50 -0600

    Ya funciona choque de clases

--- admin/matricula/insertmatricula.php
+++ admin/matricula/insertmatricula.php
@@ -5,14 +5,28 @@ include('../../conexion.php');
 	$idalumno=$_POST['idalumno'];
 	$nombre=$_POST['nombre']; //id de la materia
 
-	//prueba para que no choquen las clases, hay que hacer las variables de insertmaterias globales?????
-	/*
+	//Primera consulta para obtener la hora y dia de la materia
+
+	$cdh="SELECT horainicio as inicio, horfinal as final,dia as dia from hora_materia where idmateria='$nombre';";
+	$consulta = mysqli_query($conexion, $cdh);
+	$array = mysqli_fetch_array($consulta);
+	$horainicio= $array['inicio'];
+	$horafinal= $array['final'];
+	$dia= $array['dia'];
+
+
+	
 	$sinchoque = "SELECT count(*) as sinchoque from materias, hora_materia, materias_alumnos
 	where materias.idmateria = hora_materia.idmateria and materias.idmateria= materias_alumnos.idmateria
-	and hora_materia.horainicio ='10' and hora_materia.horfinal='12' and hora_materia.dia='Martes'  and materias_alumnos.idalumno='$idalumno';"
-	*/
+	and hora_materia.horainicio ='$horainicio' and hora_materia.horfinal='$horafinal' and hora_materia.dia='$dia' 
+	 and materias_alumnos.idalumno='$idalumno'";
+	
+	$consultad = mysqli_query($conexion, $sinchoque);
+	$arrayd = mysqli_fetch_array($consultad);
 
-	//hacemos la sentencia de sql
+	if($arrayd['sinchoque']==0){
+		
+		//hacemos la sentencia de sql
 	$sql="INSERT INTO materias_alumnos(idalumno, idmateria) VALUES('$idalumno','$nombre')";
 	//verificamos la ejecucion
 	if(mysqli_query($conexion, $sql)){
@@ -23,4 +37,11 @@ include('../../conexion.php');
 		header("Location: http://localhost:8080/formulario/admin/matricula/matricula.php");
 		
 	}
+	}
+	else 
+	{
+		echo "La clase choca xd";
+	}
+
+	
 ?>
