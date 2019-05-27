e543ec284b7a2bf63ea68b37a21dff5960d02606
Marco Narváez <marcosnarvaez2010@gmail.com>
2019-05-27 13:19:06 -0600

    ya repare bien los pop ups

--- pop-up.js
+++ pop-up.js
@@ -1,13 +1,13 @@
 $(".pop-up-activate").click(function(){
     $("body").css("pointer-events", "none");
-    $(".pop-up").css("display", "block")
+    $(".pop-up").css("display", "flex")
     console.log("Esto si funciona")
     console.log($('.pop-up')[0]);
     $(".pop-up").slideDown(500);
 });
 $(".pop-up-del").click(function(){
     $("body").css("pointer-events", "none");
-    $(".pop-up-borrar").css("display", "block")
+    $(".pop-up-borrar").css("display", "flex")
     console.log("Esto si funciona")
     console.log($('.pop-up-borrar')[0]);
     $(".pop-up-borrar").slideDown(500);
