b24c6a06bf2c015ba16fb53e19d19c7bd1eec8f5
Raandino <aandino186@gmail.com>
2019-05-28 17:23:33 -0600

    Agrege Index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Class 2.0</title>
+</head>
+<body>
+    
+<form action="Login/login.php">
+    <h1>Funciono devuelta </h1>
+    <td colspan="2" align="center"><input type="submit" name="enviando" id="enviando" value="entar"></td>
+    </form>
+
+</body>
+</html>
